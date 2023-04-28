54f7642eb40f7058e7a9f0cf08b2b4345f8c27ea
Dai Duong Nguyen <daiduong.nguyen@softwareone.com>
2023-04-28 12:15:19 +0200

    try to update position

--- src/OSMap.tsx
+++ src/OSMap.tsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component, useRef } from 'react'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
-import L from 'leaflet';
+import L, { LatLngExpression } from 'leaflet';
 import "leaflet/dist/leaflet.css";
 import PinSvg from "./mappin.svg"
 import { Chip } from '@mui/material';
@@ -11,13 +11,31 @@ export default class OSMap extends Component {
         iconSize: [48, 48],
         iconAnchor: [16, 38]
     });
-    myLocation = {lat: 52.51629936526891, lng: 13.377686483224936}; // Brandenburger Tor, Berlin
+    myLocation: LatLngExpression = {lat: 52.51629936526891, lng: 13.377686483224936}; // Brandenburger Tor, Berlin
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.myLocation = {lat: position.coords.latitude, lng: position.coords.longitude};
+                console.log(`found ${this.myLocation}`);
+                //let mapRef = useRef<L.Map>();
+                //let m = mapRef.current;
+                //m?.panTo(this.myLocation);
+                this.forceUpdate();
+            },
+            (error) => {
+                console.log(error);
+            }
+        );
+        console.log(`mount ${this.myLocation}`);
+    }
 
   render() {
+    console.log(`render ${this.myLocation}`);
     return (
-        <div>
+        <div id="map">
             <Chip label="OpenStreet Map" style={{margin: "10px"}} clickable={true} onClick={ () => {window.location.href = "/"} } />
-            <MapContainer style={{height: "980px"}} center={this.myLocation} zoom={16} scrollWheelZoom={true}>
+            <MapContainer style={{height: "980px"}} center={this.myLocation} zoom={16} scrollWheelZoom={true}
+            >
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
