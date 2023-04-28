71752afce81991fa1cf5878450076f5e4545a362
Dai Duong Nguyen <daiduong.nguyen@softwareone.com>
2023-04-28 18:02:11 +0200

    styling fireworks

--- src/FireworksPage.tsx
+++ src/FireworksPage.tsx
@@ -1,16 +1,18 @@
 // needed package: npm install @fireworks-js/react
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { Fireworks } from '@fireworks-js/react'
 import type { FireworksHandlers } from '@fireworks-js/react'
 import { Button } from '@mui/material';
+import "./FireworksPage.css"
 
 export default function FireworksPage() {
+  const [isActive, setActive] = useState(false);
   const ref = useRef<FireworksHandlers>(null)
 
   const toggle = () => {
     if (!ref.current) return
     if (ref.current.isRunning) {
-      ref.current.stop()
+      ref.current.pause()
     } else {
       ref.current.start()
     }
@@ -19,26 +21,23 @@ export default function FireworksPage() {
   return (
     <>
       <div
+        className={isActive ? "menu-active":"menu-normal"}
         style={{ display: 'flex', gap: '4px', position: 'absolute', zIndex: 1 }}
+        onMouseEnter={()=>setActive(true)}
+        onMouseOut={()=>setActive(false)}
       >
         <Button 
             variant="contained" 
             color="primary"
             size="small"
-            onClick={() => toggle()}
-        >Toggle</Button>
+            onClick={() => {window.location.href = "/"}}
+        >Home</Button>
         <Button 
             variant="contained" 
             color="primary"
             size="small"
             onClick={() => ref.current!.clear()}
           >Clear</Button>
-        <Button 
-            variant="contained" 
-            color="secondary"
-            size="small"
-            onClick={() => window.location.href = "/" }
-          >Home</Button>
       </div>
       <Fireworks
         ref={ref}
@@ -51,6 +50,7 @@ export default function FireworksPage() {
           position: 'fixed',
           background: '#000'
         }}
+        onClick={() => toggle()}
       />
     </>
   )
